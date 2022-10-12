080a5c3789db8aa0edc73f7f5b9db408efc8e4f6
Lauris DUPOIS <44022947+REWA54@users.noreply.github.com>
2022-10-12 22:06:51 +0200

    12/10
    Creating a Rail Tower and improving the desig of Shop and Upgrade stand

--- CASES2022/Assets/Scripts/LevelManager.cs
+++ CASES2022/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
@@ -8,24 +9,33 @@ public class LevelManager : MonoBehaviour
     public List<GameObject> Enemys;
     public int level;
     [SerializeField] EnemySpawner enemySpawner;
+    [SerializeField] TMP_Text moneyUI;
     float money;
     // Start is called before the first frame update
     void Start()
     {
         money = 100f;
+        UpdateUI();
+        
     }
     public void LevelStart()
     {
         enemySpawner.StartSpawn();
     }
 
+    private void UpdateUI()
+    {
+        moneyUI.text = money.ToString();
+    }
     public bool Buy(float price)
     {
         if (money >= price)
         {
             money -= price;
+            UpdateUI();
             return true;
         }
+        UpdateUI();
         return false;
     }
 }
