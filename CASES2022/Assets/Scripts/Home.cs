c3322202a2beea161cdb25a0400a7c59aab35960
Lauris DUPOIS <44022947+REWA54@users.noreply.github.com>
2022-10-28 20:11:57 +0200

    Add home life UI

--- CASES2022/Assets/Scripts/Home.cs
+++ CASES2022/Assets/Scripts/Home.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 public class Home : MonoBehaviour
 {
     public float Life;
+    public float CurrentLife;
     LevelManager levelManager;
     public GameObject homeGameObject;
+    public GameObject UIHome;
+    public Image LifeFillAmount;
+
 
     private void Start()
     {
         levelManager = FindObjectOfType<LevelManager>();
+        CurrentLife = Life;
+        UpdateUI();
+        
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,17 +33,26 @@ public class Home : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
-
+    private void Update()
+    {
+        UIHome.transform.LookAt(Camera.main.transform);
+    }
     void TakeDamages(float Damages)
     {
-        Life -= Damages;
-        if (Life <= 0)
+        CurrentLife -= Damages;
+        if (CurrentLife <= 0)
         {
             Die();
         }
         homeGameObject.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+        UpdateUI();
     }
+    void UpdateUI()
+    {
+        UIHome.GetComponentInChildren<TextMeshProUGUI>().text = CurrentLife.ToString();
+        LifeFillAmount.fillAmount = CurrentLife / Life;
 
+    }
     void Die()
     {
         levelManager.LevelEnd();
